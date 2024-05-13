47708ff8e51556704efc8318329a3919d8c58e5d
Lezcano Tech  <olivertwist200054@gmail.com>
2024-05-13 10:43:37 +0100

    Create store.component.ts

---
+++ src/app/pages/store/store.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LayoutsComponent } from '../../components/layouts/layouts.component';
+
+@Component({
+  selector: 'app-store',
+  standalone: true,
+  imports: [LayoutsComponent],
+  templateUrl: './store.component.html',
+  styleUrl: './store.component.css'
+})
+export class StoreComponent {
+
+}
