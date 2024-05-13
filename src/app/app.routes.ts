ae747e7dfd2154bff6141f65ba73d24efa726431
Lezcano Tech  <olivertwist200054@gmail.com>
2024-05-13 13:46:59 +0100

    changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,35 @@ import { StoreComponent } from './pages/store/store.component';
 import { LayoutsComponent } from './components/layouts/layouts.component';
 
 export const routes: Routes = [
+  
+    // {
+    //     path: 'store', component: StoreComponent
+    // },
     {
-        path: '', redirectTo: 'home',pathMatch: 'full'
+        path: 'home', component: HomeComponent
     },
     {
-        path: '', component: LayoutsComponent,
-        children: [
-            {
-                path: 'home', component: HomeComponent
-            },
-            {
-                path: 'store', component: StoreComponent
-            },
-        ]
+        path: 'store', component: StoreComponent
+    },
+
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full'
     }
+
+
+    // {
+    //     path: '', component: LayoutsComponent,
+    //     children: [
+    //         {
+    //             path: '', redirectTo: 'home', pathMatch: 'full'
+    //         },
+    //         {
+    //             path: 'home', component: HomeComponent
+    //         },
+    //         {
+    //             path: 'store', component: StoreComponent
+    //         }
+    //     ]
+    // }
 ];
+
