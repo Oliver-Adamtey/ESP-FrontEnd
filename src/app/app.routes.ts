10f48646563bf07a3fc3d402082e9ff0220c8d63
Lezcano Tech  <olivertwist200054@gmail.com>
2024-05-13 10:43:20 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { StoreComponent } from './pages/store/store.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'home',pathMatch: 'full'
+    },
+    {
+        path: '', component: LayoutsComponent,
+        children: [
+            {
+                path: 'home', component: HomeComponent
+            },
+            {
+                path: 'store', component: StoreComponent
+            },
+        ]
+    }
+];
