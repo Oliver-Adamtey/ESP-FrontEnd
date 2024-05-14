5a99838cb4ab7316d5e3db1f4a9e0a2eccf4a8fb
Lezcano Tech  <olivertwist200054@gmail.com>
2024-05-14 12:57:22 +0100

    Update store.component.ts

--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LayoutsComponent } from '../../components/layouts/layouts.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-store',
   standalone: true,
-  imports: [LayoutsComponent],
+  imports: [LayoutsComponent, CommonModule],
   templateUrl: './store.component.html',
   styleUrl: './store.component.css'
 })
