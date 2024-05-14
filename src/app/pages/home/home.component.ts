1d82a1111828b306172ed2c0e6254b39e6e1b691
Lezcano Tech  <olivertwist200054@gmail.com>
2024-05-14 12:57:25 +0100

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LayoutsComponent } from '../../components/layouts/layouts.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LayoutsComponent],
+  imports: [CommonModule, LayoutsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
