dc6d5e60ad83b184711a5d717d7ea192ff9d455d
Lezcano Tech  <olivertwist200054@gmail.com>
2024-05-16 07:02:16 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { StoreComponent } from './pages/store/store.component';
 import { LayoutsComponent } from './components/layouts/layouts.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 export const routes: Routes = [
     {
@@ -9,7 +10,8 @@ export const routes: Routes = [
         children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'store', component: StoreComponent }
+            { path: 'store', component: StoreComponent },
+            { path: 'blog', component: BlogComponent }
         ]
     },
     { path: '**', redirectTo: '' } // fallback route
