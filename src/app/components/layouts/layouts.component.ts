8ba880ae78da05622600a8fe18054f6ba9c4fe56
Lezcano Tech  <olivertwist200054@gmail.com>
2024-05-16 07:03:03 +0100

    Update layouts.component.ts

--- src/app/components/layouts/layouts.component.ts
+++ src/app/components/layouts/layouts.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layouts',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './layouts.component.html',
   styleUrl: './layouts.component.css'
 })
 export class LayoutsComponent {
+  sidebarOpen = false;
 
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
